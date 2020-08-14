0244f8b97ed1cf65db7b588a4f9bb7ee0a429c66
Shivansh Saini <shivanshs9@gmail.com>
2020-08-14 12:38:51 +0530

    hack: added default value test
    - the test passes but default value functionality doesn't really work since kotlinpoet hasn't yet implemented it completely
    
    Signed-off-by: Shivansh Saini <shivanshs9@gmail.com>

--- ergo-processor/src/test/kotlin/ergo/KotlinTaskTest.kt
+++ ergo-processor/src/test/kotlin/ergo/KotlinTaskTest.kt
@@ -167,6 +167,27 @@ class KotlinTaskTest {
         assertResult(result)
     }
 
+    // Default Value is not yet supported in kotlinpoet so the
+    // generated request data class is actually invalid
+    @Test
+    fun oneArgWithDefaultValue() {
+        val source = """
+            package example.tasks
+            
+            import headout.oss.ergo.annotations.Task
+            
+            class ExampleTask {
+                @Task(taskId="oneArgWithDefaultValue")
+                fun oneArgWithDefaultValue(num: Int = 134) {
+                    println("doing some work x $\num...")
+                }
+            }
+        """.trimIndent()
+
+        val result = compile(source)
+        assertResult(result)
+    }
+
     private fun assertResult(result: KotlinCompilation.Result, exitCode: ExitCode = ExitCode.OK) {
         result.sourcesGeneratedByAnnotationProcessor.forEach {
             println(it.canonicalPath)
