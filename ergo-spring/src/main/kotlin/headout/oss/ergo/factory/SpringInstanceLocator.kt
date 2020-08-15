a765c6a557f0a95a0e437c2ee776d580c17537a2
Shivansh Saini <shivanshs9@gmail.com>
2020-08-15 21:03:53 +0530

    fix: check and throw error when creating spring bean
    Signed-off-by: Shivansh Saini <shivanshs9@gmail.com>

--- ergo-spring/src/main/kotlin/headout/oss/ergo/factory/SpringInstanceLocator.kt
+++ ergo-spring/src/main/kotlin/headout/oss/ergo/factory/SpringInstanceLocator.kt
@@ -7,6 +7,10 @@ import kotlin.reflect.KClass
  * Created by shivanshs9 on 13/07/20.
  */
 class SpringInstanceLocator : InstanceLocator {
-    override fun <T : Any> getInstance(clazz: KClass<T>): T? =
-        kotlin.runCatching { SpringContext.getBean(clazz) }.getOrNull()
+    override fun <T : Any> getInstance(clazz: KClass<T>): T? {
+        val isSpringBean = clazz.annotations.filter {
+            it.annotationClass.qualifiedName?.contains("org.springframework") ?: false
+        }.isNotEmpty()
+        return if (isSpringBean) SpringContext.getBean(clazz) else null
+    }
 }
